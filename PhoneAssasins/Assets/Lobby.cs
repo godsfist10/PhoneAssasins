07429d82565cb6837f390e16c968d666eec8bc06
paul <paul.kubik@mymail.champlain.edu>
2015-03-23 03:47:44 -0400

    added aws stuff

---
+++ PhoneAssasins/Assets/Lobby.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using Amazon;
+
+public class Lobby : MonoBehaviour 
+{
+    
+
+
+}
---
+++ PhoneAssasins/Assets/Profile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Profile : MonoBehaviour {
+
+    public string _Name;
+    public Texture2D _Picture; //maybe
+    public string _Bio;
+    public int _KillCount;
+    public int _TotalKillCount;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        _KillCount = 0;
+        _Name = "";
+        _Bio = "";
+
+	}
+	
+    void Init(string name)
+    {
+        _Name = name;
+    }
+}
